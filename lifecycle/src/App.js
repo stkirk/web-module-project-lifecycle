ce90d8b2a15741471509c28626f132ae31bb13ff
stkirk <shaanekirk@gmail.com>
2021-08-07 12:46:44 -0400

    scaffolded App component and transitioned it to a class component, made axios request for user profile data

--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {},
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/stkirk")
+      .then((res) => console.log("GitHub Response", res.data));
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>gitHub User</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
